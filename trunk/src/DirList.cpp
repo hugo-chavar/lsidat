3b187df83809f2a49ae41fcb9c99a31657f13069
hechavar <hechavar@a0a87f8a-4cd0-0856-6464-24e5c74081ae>
2012-09-07 21:47:37 +0000

    Termino la clase DirList, queda un main para mostrar como funciona.

--- trunk/src/DirList.cpp
+++ trunk/src/DirList.cpp
@@ -10,6 +10,8 @@ using namespace std;
 
 DirList::DirList() {
 	// TODO Auto-generated constructor stub
+	cantidad = 0;
+	_posicionActual=0;
 
 }
 
@@ -17,8 +19,8 @@ DirList::~DirList() {
 	// TODO Auto-generated destructor stub
 }
 
-bool DirList::create(string dir) {
-	string filepath;
+bool DirList::crearDesdeDirectorio(string dir) {
+	string filepath, filename;
 	//ifstream fin; por si necesito abrir los archivos
 	DIR *dp;
 	struct dirent *dirp;
@@ -28,15 +30,20 @@ bool DirList::create(string dir) {
 		cout << "Se produjo un error al abrir el directorio " << dir << endl;
 		return false;
 	}
+	this->directorio = dir;
 
 	while ((dirp = readdir(dp))) {
 		filepath = dir + "/" + dirp->d_name;
+
 
 		// Si hay directorios o cosas raras las ignora
 		if (stat(filepath.c_str(), &filestat))
 			continue;
 		if (S_ISDIR( filestat.st_mode ))
 			continue;
+		filename = dirp->d_name;
+		archivos.push_back(filename);
+		cantidad++;
 
 		//por ahora solo muestro los nombres de archivos completos
 		cout << filepath << endl;
@@ -47,8 +54,49 @@ bool DirList::create(string dir) {
 //		      cout << filepath << ": " << num << endl;
 //		    fin.close();
 	}
+	//queda ordenado por nombre de archivo
+	archivos.sort();
+	//dejo apuntando al primero
+	iterador = archivos.begin();
 
 	closedir(dp);
 	return true;
 
+}
+
+string DirList::siguiente(){
+	string sigte = *iterador;
+	iterador++;
+	_posicionActual++;
+
+	return sigte;
+
+}
+
+string DirList::siguienteLargo(){
+	string sigte = directorio+"/"+(*iterador);
+	iterador++;
+	_posicionActual++;
+
+	return sigte;
+
+}
+bool DirList::haySiguiente(){
+	return (_posicionActual<cantidad);
+}
+
+bool DirList::seek(unsigned pos){
+	bool status = (pos<cantidad);
+	if(status){
+		_posicionActual=pos;
+		iterador = archivos.begin();
+		for (unsigned i=0;i<pos;i++){
+			iterador++;
+		}
+	}
+	return status;
+}
+
+unsigned DirList::posicionActual(){
+	return _posicionActual;
 }
