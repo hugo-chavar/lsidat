77cc21b5a4570a74acad6b89d4bf7bb1101a3184
andresg.sanabria <andresg.sanabria@a0a87f8a-4cd0-0856-6464-24e5c74081ae>
2012-09-13 14:59:41 +0000

    Subo las clases que en principio necesito para mi parte. Faltan implementar bastantes metodos

---
+++ trunk/src/InfoPalabra.h
@@ -0,0 +1,29 @@
+/*
+ * InfoPalabra.h
+ *
+ *  Created on: Sep 8, 2012
+ *      Author: andy
+ */
+
+#ifndef INFOPALABRA_H_
+#define INFOPALABRA_H_
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class InfoPalabra {
+
+private:
+	unsigned documento;
+	unsigned cantidad;
+public:
+	InfoPalabra(unsigned doc,unsigned cant);
+	~InfoPalabra();
+	unsigned getCantidad();
+	void incrementarCantidad();
+	unsigned getDocumento();
+};
+
+#endif /* INFOPALABRA_H_ */
---
+++ trunk/src/Palabra.h
@@ -0,0 +1,30 @@
+/*
+ * Palabra.h
+ *
+ *  Created on: Sep 8, 2012
+ *      Author: andy
+ */
+
+#ifndef PALABRA_H_
+#define PALABRA_H_
+
+#include "InfoPalabra.h"
+#include <list>
+
+using namespace std;
+
+class Palabra {
+
+private:
+	string contenido;
+	list<InfoPalabra> informacion;
+
+public:
+	Palabra(string cont);
+	~Palabra();
+	void agregarAparicion(unsigned doc);
+	InfoPalabra* buscarDoc(unsigned doc);
+
+};
+
+#endif /* PALABRA_H_ */
