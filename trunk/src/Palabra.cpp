b0023175765e2e77969c67d448e3b1d0f0c313d5
hechavar <hechavar@a0a87f8a-4cd0-0856-6464-24e5c74081ae>
2012-09-14 13:25:15 +0000

    pongo un metodo agregarNodo en palabra, y leerLinea en archivo

--- trunk/src/Palabra.cpp
+++ trunk/src/Palabra.cpp
@@ -9,35 +9,37 @@
 
 Palabra::Palabra(string cont) {
 
-	this->contenido=cont;
+	this->contenido = cont;
 }
 
 Palabra::~Palabra() {
 	// TODO Auto-generated destructor stub
 }
 
-void Palabra::agregarAparicion(unsigned doc){
+void Palabra::agregarAparicion(unsigned doc) {
 
-	InfoPalabra* nodo= this->buscarDoc(doc);
-	if(nodo)
-	nodo->incrementarCantidad();
-	else
-	{
-		this->informacion.push_back(*(new InfoPalabra(doc,1)));
+	InfoPalabra* nodo = this->buscarDoc(doc);
+	if (nodo)
+		nodo->incrementarCantidad();
+	else {
+		//this->informacion.push_back(*(new InfoPalabra(doc,1)));
+		this->agregarNodo(doc, 1);
 	}
 }
 
-InfoPalabra* Palabra::buscarDoc(unsigned doc){
-	list<InfoPalabra>::iterator it= this->informacion.begin();
-	bool encontrado=false;
-	while((&(*it)) && (!encontrado))
-	{
-	if(it->getDocumento()==doc){
-		encontrado=true;
-		}
-	else{
-		advance(it,1);
+InfoPalabra* Palabra::buscarDoc(unsigned doc) {
+	list<InfoPalabra>::iterator it = this->informacion.begin();
+	bool encontrado = false;
+	while ((&(*it)) && (!encontrado)) {
+		if (it->getDocumento() == doc) {
+			encontrado = true;
+		} else {
+			advance(it, 1);
 		}
 	}
 	return (&(*it));
+}
+
+void Palabra::agregarNodo(unsigned doc, unsigned cantidad) {
+	this->informacion.push_back(*(new InfoPalabra(doc, cantidad)));
 }
