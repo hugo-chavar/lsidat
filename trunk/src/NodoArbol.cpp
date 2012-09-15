0fa71adfb9392a3bf45655430f67458f7c915031
andresg.sanabria <andresg.sanabria@a0a87f8a-4cd0-0856-6464-24e5c74081ae>
2012-09-15 22:49:19 +0000

    Avanzo con el arbol... Me falta solo el recorrido in-Order

---
+++ trunk/src/NodoArbol.cpp
@@ -0,0 +1,50 @@
+/*
+ * NodoArbol.cpp
+ *
+ *  Created on: Sep 14, 2012
+ *      Author: andy
+ */
+
+#include "NodoArbol.h"
+
+NodoArbol::NodoArbol(string palabra){
+	this->dato=new Palabra(palabra);
+	this->derecho=NULL;
+	this->izquierdo=NULL;
+}
+
+NodoArbol::~NodoArbol() {
+	delete dato;
+}
+
+NodoArbol* NodoArbol::getIzquierdo(){
+	return(this->izquierdo);
+}
+
+NodoArbol* NodoArbol::getDerecho(){
+	return(this->derecho);
+}
+
+void NodoArbol::setIzquierdo(NodoArbol* izq){
+	this->izquierdo=izq;
+}
+
+void NodoArbol::setDerecho(NodoArbol* der){
+	this->derecho=der;
+}
+
+Palabra NodoArbol::getPalabra(){
+	return(*(this->dato));
+}
+
+
+int NodoArbol::CompararCon(string clave){
+	if ((this->dato->getContenido())<clave)
+		return (-1);
+	else if((this->dato->getContenido())==clave)
+			return (0);
+		else
+			return (1);
+}
+
+
